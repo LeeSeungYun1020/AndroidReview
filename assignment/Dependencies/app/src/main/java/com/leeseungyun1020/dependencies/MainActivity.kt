165c230a84e43450abcb9d4bec748daf3ca140bf
Lee Seung Yun <ileilliat@gmail.com>
2023-02-08 14:18:46 +0900

    assignment - Dependency 3

--- assignment/Dependencies/app/src/main/java/com/leeseungyun1020/dependencies/MainActivity.kt
+++ assignment/Dependencies/app/src/main/java/com/leeseungyun1020/dependencies/MainActivity.kt
@@ -6,26 +6,20 @@ import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.leeseungyun1020.dependencies.ui.theme.DependenciesTheme
-import com.leeseungyun1020.sub1.Wiki
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.leeseungyun1020.dependencies.ui.theme.DependenciesTheme
 import com.leeseungyun1020.sub1.FourImage
+import com.leeseungyun1020.sub1.Wiki
 import com.leeseungyun1020.sub2.MyImage
 
 class MainActivity : ComponentActivity() {
